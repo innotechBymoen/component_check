9342148d6378aef438d527c32d900340d6f4e9dc
bymoen <alexb@innotechcollege.com>
2023-04-13 19:33:47 -0600

    Created a header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>LOGO</h1>
+        <nav>
+            <p>LINK ONE</p>
+            <p>LINK TWO</p>
+            <p>LINK THREE</p>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div{ 
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+        justify-items: start;
+    }
+
+    nav {
+        display: grid;
+        justify-self: end;
+        grid-auto-flow: column;
+    }
+</style>
